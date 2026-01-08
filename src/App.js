585892fce887aa63519db25fe35517ad9082805d
Pongsakorn-Siritemkul <s6703052411295@email.kmutnb.ac.th>
2026-01-08 11:34:15 +0700

    Calculator
    Calculator

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import Button from './class-components';
+import Calculator from './calculator';
 
-function App() {
-  return <Button />;
+export default function App() {
+  return <Calculator />;
 }
-export default App;
